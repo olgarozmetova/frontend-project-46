267b2bb57b223747c1944b2d531bd6a7a0067cda
olga rozmetova <olga.rozmetova@gmail.com>
2025-09-09 16:42:55 +0200

    added arg and opt to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,10 @@ const genDiff = () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
 
-  program.parse(process.argv)
+  program.parse()
 }
 
 genDiff()
