7b4dc3bd5e5f539c4de335fbf7d360ec0a218489
olga rozmetova <olga.rozmetova@gmail.com>
2025-09-09 16:12:43 +0200

    created gendiff.js

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 install:
 	npm ci
 
+gendiff:
+	node bin/gendiff.js	
+
 lint:
 	npx eslint .
 
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+import { program } from 'commander'
+
+const genDiff = () => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+
+  program.parse(process.argv)
+}
+
+genDiff()
+
+export default genDiff
