6b7bc0be2aca2bfe9cae150ac1d9655089cb4abb
olga rozmetova <olga.rozmetova@gmail.com>
2025-09-27 19:09:47 +0200

    added lint script

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,9 +6,10 @@ import genDiff from '../src/genDiff.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const getFixturePath = (filename) =>
+const getFixturePath = filename =>
   path.join(__dirname, '..', '__fixtures__', filename)
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8')
+
+const readFile = filename => readFileSync(getFixturePath(filename), 'utf-8')
 
 test('gendiff flat json', () => {
   const file1 = JSON.parse(readFile('file1.json'))
